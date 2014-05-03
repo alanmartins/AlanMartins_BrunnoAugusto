7c2afe167737446c68b5a793f2853f6ffb184fa7
Alan Martins <martins034156@yahoo.com.br>
2014-05-03 18:32:05 -0300

    Sem verificação de erros léxicos

--- AlanMartins_BrunnoAugusto/src/main/Compiler.java
+++ AlanMartins_BrunnoAugusto/src/main/Compiler.java
@@ -1,25 +1,36 @@
 package main;
 
-import parser.*;
-import lexer.*;
-import node.*;
-
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PushbackReader;
 
 import org.sablecc.sablecc.lexer.Lexer;
+import org.sablecc.sablecc.lexer.LexerException;
+import org.sablecc.sablecc.node.Token;
 
 public class Compiler {
     
      public static void main(String[] arguments) {
-         try {
-               System.out.println("Type an arithmetic expression:");
-            
-               // Create a Parser instance.
-               Lexer l = new Lexer( new PushbackReader( new InputStreamReader(System.in), 1024));
-            
-         }
-         catch(Exception e) {
-               System.out.println(e.getMessage());
-         }
+          try {
+        
+             FileInputStream file = new FileInputStream(new File("C:\\Users\\Alan Martins\\Documents\\GitHub\\AlanMartins_BrunnoAugusto\\src\\doc\\teste.txt"));
+             Lexer lex =  new Lexer( new PushbackReader(new InputStreamReader(file)));
+
+             analiseLexica(lex);
+             
+          }
+          catch(Exception e) {
+           System.out.println(e.getMessage());
+          }
      }
+     
+     
+     public static void analiseLexica(Lexer lexer) throws LexerException, IOException{
+       
+         System.out.println(lexer.next());
+         
+     }
+     
 }
